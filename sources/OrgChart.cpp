ffa889008863b5d4c42fd37b713fd74228ac7921
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2022-05-25 12:35:39 +0300

    Add comments to code

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -1,25 +1,38 @@
 #include "OrgChart.hpp"
 using namespace std;
 namespace ariel {
+    //---------------------------------------------------
+    //-----------------adding functions------------------
     OrgChart &OrgChart::add_root(string name) {
+        /***
+         * Gets one input and puts it at the root of the tree. If there is already something in the root, it is replaced.
+         */
         if (name.empty() || name == "\t"|| name == "\r" || name == "\n"|| name == " " ){
-            throw invalid_argument("One or both of the strings are invalid");
+            throw invalid_argument("The string is invalid");
         }
         this->root.Name = move(name);
         return *this;
     }
 
     OrgChart &OrgChart::add_sub(const string &nameONE, const string &nameTWO) {
+        /***
+         * add_sub receives two inputs. A person (or department) that already exists in the organizational structure and another person
+         * (or another department) who report to the first person and are below him in the organizational hierarchy.
+         */
         if (nameONE.empty() || nameTWO.empty() || nameONE == "\t" || nameTWO == "\t"|| nameTWO == "\r" || nameONE == "\r"|| nameTWO == "\n" || nameTWO == "\n"|| nameONE == " " || nameTWO == " " ){
             throw invalid_argument("One or both of the strings are invalid");
         }
         if (!LookForNode(&this->root, nameONE, nameTWO)) {
-            throw runtime_error("The first person the Knesset does not exist in the organization tree!") ;
+            throw runtime_error("The first person you entered does not exist in the organization tree!") ;
         }
         return *this;
     }
-
+    //---------------------------------------------------------------
+    //---------Auxiliary functions for insertion functions-----------
     bool OrgChart::LookForNode(Node *root, const string &father,const string &son) {
+        /***
+         * An auxiliary function that reports to the first person whether the second person is in the organizational hierarchy.
+         */
         bool ans= false;
         if(root->Name==father) { //check it the father is the root
             AddNode(root, father,son);
@@ -34,13 +47,16 @@ namespace ariel {
         return ans;
     }
     void OrgChart::AddNode(Node *root, const string &father, const string &son) {
+        /***
+         * If the person is indeed in the organizational hierarchy we will add him to this OrgChart
+         */
         Node* temp=new Node();
         temp->Name=son;
         root->Sons.push_back(temp);
         this->children.push_back(temp);
     }
-
-
+    //------------------------------------------------------------
+    //------------------------Iterators---------------------------
     iterator OrgChart::begin_level_order() {
         if (this->root.Name.empty()) {
             throw runtime_error("This chart not exist");
@@ -96,7 +112,12 @@ namespace ariel {
         }
         return iterator();
     }
-     string& OrgChart::print(string& oldString, const string& start, Node *node){
+    //-----------------operator<< and print-----------------------
+
+    string& OrgChart::print(string& oldString, const string& start, Node *node){
+        /***
+         * The print function will go over the entire root, for each son you will perform a Tab
+         */
         oldString += start;
         if(!oldString.empty()){oldString += "->";}
         oldString = oldString+ node->Name+"\n";
--- sources/iterator.hpp
+++ sources/iterator.hpp
@@ -13,16 +13,16 @@ namespace ariel {
 
     class iterator {
     private:
-        Node *pointer_to_current_node;
+        Node *pointer_to_current_node; //The name is taken from the lecture
         string state;
         queue<Node*> levelorder;
         vector<Node*>preorder;
         vector<Node*> reverse;
         size_t index = 0;
     public:
-        //----------------------------------------
+        //--------------------------------------------------------------------------------------------------------------
         // constructors
-        //----------------------------------------
+        //--------------------------------------------------------------------------------------------------------------
         iterator():pointer_to_current_node(){}
         iterator(Node* ptr,string State):pointer_to_current_node(ptr){
             this->state= std::move(State);
@@ -37,9 +37,9 @@ namespace ariel {
                 this->preorder.push_back(ptr);
             }
         }
-        //----------------------------------------
+        //--------------------------------------------------------------------------------------------------------------
         // operators
-        //----------------------------------------
+        //--------------------------------------------------------------------------------------------------------------
         string& operator*() const {
             if(this->pointer_to_current_node== nullptr){
                 throw ("The current pointer is null");
@@ -49,6 +49,10 @@ namespace ariel {
         string* operator->() const  { return &(pointer_to_current_node->Name);}
 
         iterator& operator++() {
+            /***
+             * This function checks the state of transition on the OrgChart and determines
+             * the current pointer accordingly to return the pointer instead of the ++
+             */
             this->pointer_to_current_node= nullptr;
             if(state=="LevelOrder") {
                 LevelOrderTable();
@@ -79,10 +83,14 @@ namespace ariel {
             return one.pointer_to_current_node != two.pointer_to_current_node;
         }
 
-        //----------------------------------------
+        //--------------------------------------------------------------------------------------------------------------
         // Operator Auxiliary Functions
-        //----------------------------------------
+        //--------------------------------------------------------------------------------------------------------------
         void PreorderTable(){
+            /***
+             * This function takes the sons of the root and inserts them in the order of Preorder
+               explain: We go through the whole child from the top of the root from right to left, and put the child in a pile
+             */
             Node* curr=this->preorder.at(this->preorder.size()-1);
             this->preorder.pop_back();
             for(int i=int(curr->Sons.size()-1);i>=0;--i){
@@ -90,12 +98,21 @@ namespace ariel {
             }
         }
         void LevelOrderTable(){
+            /***
+             * This function takes the sons of the root and inserts them in the order of by level
+               explain: In this function we take NODE each time and put its children in the queue
+             */
             Node *curr = this->levelorder.front();
             for (size_t i = 0; i < curr->Sons.size(); ++i) {
                 this->levelorder.push(curr->Sons.at(i));
             }
         }
         void ReverseTable(Node* ptr){
+            /***
+              This function takes the sons of the root and keeps them in the vector of the NODES of the class in reverse order
+              explain: In this function we get a root, push it a temporary locator, then go over each of its children and put them in reverse order.
+                       Finally, we will copy the class' temporary vector
+             */
             this->reverse.clear();
             vector<Node*> temp_vector;
             temp_vector.push_back(ptr);
